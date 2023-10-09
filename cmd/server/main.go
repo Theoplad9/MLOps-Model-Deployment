4ef9d9e11a06a59802e2fff6fe567a56271cd226
Dr. Ethan Vance <mcinunmorcornpo@gmx.net>
2023-10-09 09:21:17 -0400

    feat: Implement basic model serving API with health check

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,22 +1,108 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/Theoplad9/MLOps-Model-Deployment/internal/handler"
-	"github.com/Theoplad9/MLOps-Model-Deployment/internal/model"
 )
 
+type MLModel interface {
+	Predict([]float64) (float64, error)
+	Load(string) error
+}
+
+type SimpleLinearModel struct {
+	weights []float64
+	bias    float64
+}
+
+func (slm *SimpleLinearModel) Predict(features []float64) (float64, error) {
+	if len(features) != len(slm.weights) {
+		return 0, fmt.Errorf("feature count mismatch: expected %d, got %d", len(slm.weights), len(features))
+	}
+	prediction := slm.bias
+	for i, f := range features {
+		prediction += f * slm.weights[i]
+	}
+	return prediction, nil
+}
+
+func (slm *SimpleLinearModel) Load(modelPath string) error {
+	log.Printf("Loading model from %s...", modelPath)
+	time.Sleep(1 * time.Second)
+	slm.weights = []float64{0.5, 1.2, -0.3}
+	slm.bias = 0.1
+	log.Println("SimpleLinearModel loaded successfully.")
+	return nil
+}
+
+var globalModel MLModel
+
+func init() {
+	globalModel = &SimpleLinearModel{}
+	err := globalModel.Load("/models/dummy_model.json")
+	if err != nil {
+		log.Fatalf("Failed to load initial model: %v", err)
+	}
+}
+
+type PredictionRequest struct {
+	Features []float64 `json:"features"`
+}
+
+type PredictionResponse struct {
+	Prediction float64 `json:"prediction"`
+	Message    string  `json:"message"`
+	Error      string  `json:"error,omitempty"`
+}
+
+func PredictHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var req PredictionRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Invalid request payload: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	if len(req.Features) == 0 {
+		http.Error(w, "Features array cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	prediction, err := globalModel.Predict(req.Features)
+	if err != nil {
+		resp := PredictionResponse{Error: err.Error(), Message: "Prediction failed"}
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	resp := PredictionResponse{
+		Prediction: prediction,
+		Message:    "Prediction successful",
+	}
+	json.NewEncoder(w).Encode(resp)
+	log.Printf("Received prediction request with features: %v, responded with prediction: %f", req.Features, prediction)
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+}
+
 func main() {
-	// Load the ML model (dummy for now)
-	model.LoadModel()
-
 	r := mux.NewRouter()
-	r.HandleFunc("/predict", handler.PredictHandler).Methods("POST")
+	r.HandleFunc("/predict", PredictHandler).Methods("POST")
+	r.HandleFunc("/health", healthCheckHandler).Methods("GET")
 
-	fmt.Println("Server starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	port := ":8080"
+	fmt.Printf("Server starting on %s
+", port)
+	log.Fatal(http.ListenAndServe(port, r))
 }
